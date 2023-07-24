9050d350be88dac26e3ff9a09e09ef60304cb078
Василий Андреевич Дударевич <94409566+LightningDeath@users.noreply.github.com>
2023-07-25 01:40:32 +0300

    Add files via upload

--- First_test/presenter.py
+++ First_test/presenter.py
@@ -1,2 +1,21 @@
+from re import match
+
+import model
+import view
 
 
+def run():
+    choice = view.user_choice()
+    if choice == "1":
+        note_list = view.new_note()
+        print(note_list)
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        pass
+    elif choice == 6:
+        exit(0)
--- First_test/view.py
+++ First_test/view.py
@@ -2,4 +2,22 @@ from Module import Chek_input_vol
 
 
 def user_choice():
-    print("")
+    print("\nВыберите действие:\n"
+          "1 - Добавить запись;\n"
+          "2 - Найти запись;\n"
+          "3 - Удалить запись;\n"
+          "4 - Вывести на экран все записи;\n"
+          "5 - Очистить записную книжку;\n"
+          "6 - Выход из программы.")
+    print("Ваш выбор:")
+    choice = input()
+    return choice
+
+
+def new_note():
+    print("Введите заголовок заметки:")
+    header = input()
+    print("Введите текст заметки:")
+    text = input()
+    note_list = (header, text)
+    return note_list
